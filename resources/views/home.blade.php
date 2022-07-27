dfec7038f6bfef37c4dcf80ff38bcf1dbd3dbd3f
erica201193 <erica.mancini20@gmail.com>
2022-07-27 03:03:13 +0200

    Completed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,33 @@
+@php
+$bannerLinks = [
+                [
+                    "name" => "DIGITAL COMICS",
+                    "href" => "#",
+                    "url" => "buy-comics-digital-comics.png" 
+                ],
+                [
+                    "name" => "DC MERCHANDISE",
+                    "href" => "#",
+                    "url" => "buy-comics-merchandise.png" 
+                ],
+                [
+                    "name" => "SUBSCRIPTION",
+                    "href" => "#",
+                    "url" => "buy-comics-subscriptions.png" 
+                ],
+                [
+                    "name" => "COMIC SHOP LOCATOR",
+                    "href" => "#",
+                    "url" => "buy-comics-shop-locator.png" 
+                ],
+                [
+                    "name" => "DC POWER VISA",
+                    "href" => "#",
+                    "url" => "buy-dc-power-visa.svg" 
+                ]
+];
+@endphp
+
 @extends('layouts.public')
 
 @section('page_title', "DC Comics")
@@ -23,6 +53,21 @@
                         </div>
                     @endforeach
     
+                </div>
+            </div>
+
+            <div class="bg-blue">
+                <div class="container">
+                    <div class="row text-light py-5">
+
+                        @foreach ($bannerLinks as $link)
+                            <div class="col d-flex justify-content-center align-items-center">
+                                <img src="{{ asset('images/') . '/' . $link['url'] }}" alt="">
+                                <a href="#" class="text-light text-decoration-none ps-2">{{ $link["name"] }}</a>
+                            </div>
+                        @endforeach
+
+                    </div>
                 </div>
             </div>
             
